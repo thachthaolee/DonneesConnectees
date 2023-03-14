8c0496fd23a49f8659dafd098c24f4a157d65a96
thachthaolee <thachthaole.fr@gmail.com>
2023-03-14 01:36:06 +0100

    setup the project

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const app = express();
+
+// This variable defines the port of your computer where the API will be available
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (request, response) => {
+    // The string we want to display on http://localhost:3000
+    response.send('Welcome on the annotations API! Take a breath and start using it!');
+});
+
+app.listen(PORT, () =>{
+  console.log(`The Annotations API is running on: http://localhost:${PORT}.`);
+});
+
