61d5850d01c425236b29f6983536c667c78c4cd2
thachthaolee <thachthaole.fr@gmail.com>
2023-03-15 00:32:18 +0100

    update the project

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
 
+// Import Express.js
 const express = require('express');
 const app = express();
+// Import body-parser (to hadle parameters more easily)
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 // This variable defines the port of your computer where the API will be available
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (request, response) => {
-    // The string we want to display on http://localhost:3000
-    response.send('Welcome on the annotations API! Hello Wold ! ');
+var data = {};
+var id = 0;
+
+app.use(express.static('html'));
+
+app.get("/", (request, response) => {
+  response.send("Welcome on the annotation API!");
+});
+
+app.post('/annotation', (request, response) => {
+  var body = request.body;
+  data[id]=body;
+  console.log(data);
+  id++;
+  response.send("votre annotation a été sauvegardé !");
+});
+
+app.get('/IdAnnot/:Annot', (req,res) => {
+  var IdAnnot = req.params.Annot;
+
+  var Exist = Object.keys(data).includes(IdAnnot);
+  console.log(Exist);
+
+  res.format ({
+    'text/html': function() {
+       if (Exist){
+       res.setHeader('Content-Type', 'text/html');
+         res.send("<!DOCTYPE html><html lang='fr'><head><meta charset='UTF-8'/><title>Titre</title></head><body><div>"+JSON.stringify(data[IdAnnot])+
+           "</div></body></html>"); 
+       }
+       else {
+        res.send("aucune annotation n'est associée à cette clé");
+       }
+    },
+
+    'application/json': function() {
+       if (Exist){
+         res.send(data[IdAnnot]); 
+       }
+       else {
+        res.send("aucune annotation n'est associée à cette clé");
+       }
+   }
+  });
+});
+
+app.get('/URI/:AnnotURI', (req, res) => {
+  var IdURI = req.params.AnnotURI;
+  console.log(IdURI);
+
+  var tabRep = [];
+
+  for ( key in data){
+    if (data[key]['URI']==IdURI){
+      tabRep.push({'IdAnnotation' : data[key], 'Annotation' : data[key]['Annotation']});
+
+    }
+  }
+  res.format ({
+    'text/html': function() {
+      res.setHeader('Content-Type', 'text/html');
+      res.send("<!DOCTYPE html><html lang='fr'><head><meta charset='UTF-8'/><title>Titre</title></head><body><div>"+JSON.stringify(tabRep)+
+          "</div></body></html>"); 
+     },
+
+     'application/json': function() {
+      res.send(tabRep); 
+    }
+  });
 });
 
 app.listen(PORT, () =>{
